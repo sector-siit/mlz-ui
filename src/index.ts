0f82fd89af4156e9172f692db3f42f5d47983445
plugo <plugo@andreani.com>
2024-06-22 02:18:37 -0300

    feat: :sparkles: add textArea

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import { Card } from './components/Card/Card'
 import { Alert } from './components/Alert/Alert'
 import { Button } from './components/Button/Button'
 import { TextArea } from './components/TextArea/TextArea'
-export {default} from './components/TextArea/TextArea'
-export { Card, Alert,Button, TextArea }
+export { default } from './components/TextArea/TextArea'
+export { Card, Alert, Button, TextArea }
